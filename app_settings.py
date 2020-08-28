8127932839228aebc7b8d01b1325f62afad70ce7
sergiobrr <sergio.brero.it@gmail.com>
2020-08-28 07:55:59 +0200

    Initial commit n2

---
+++ app_settings.py
@@ -0,0 +1,42 @@
+import os
+from people.app.schemas import *
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'uf^35kk6mvn*&b@@t*9$crigf1z(x)r00*#yd-pf)jw3ias9j0'
+
+# ATTENZIONE SOLO IN DEVELOP...
+X_DOMAINS = ['*']
+X_HEADERS = ['Authorization', 'Content-Type', 'If-Match']
+DEBUG = True
+
+# Please note that MONGO_HOST and MONGO_PORT could very well be left
+# out as they already default to a bare bones local 'mongod' instance.
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_DBNAME = 'primodesiderio'
+
+# Enable reads (GET), inserts (POST) and DELETE for resources/collections
+# (if you omit this line, the API will default to ['GET'] and provide
+# read-only access to the endpoint).
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+
+# Enable reads (GET), edits (PATCH), replacements (PUT) and deletes of
+# individual items  (defaults to read-only item access).
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+people = {
+    'item_title': 'Persons registered on primodesiderio.com',
+    'schema': PeopleSchema
+}
+
+desire = {
+    'item_title': 'Desires desired on primodesiderio.com',
+    'schema': DesireSchema
+}
+URL_PREFIX = 'api/v1'
+DOMAIN = {
+    'people': people,
+    'desire': desire
+}
+
